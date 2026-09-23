7ee33701f81d8bd1de613abc26dcff1751a4090c
Björn Ritzl <bjorn.ritzl@gmail.com>
2026-09-23 15:03:47 +0200

    Update photon_extension_defines.h

--- fusion/src/photon_extension_defines.h
+++ fusion/src/photon_extension_defines.h
@@ -11,13 +11,7 @@
 	#if defined(__aarch64__)
 		#define PHOTON_PLATFORM_SUPPORTED 0
 	#else
-		#define PHOTON_PLATFORM_SUPPORTED 0
-	#endif
-#elif defined(DM_PLATFORM_WINDOWS)
-	#if defined(__aarch64__)
 		#define PHOTON_PLATFORM_SUPPORTED 1
-	#else
-		#define PHOTON_PLATFORM_SUPPORTED 0
 	#endif
 #elif defined(DM_PLATFORM_ANDROID)
 	#if defined(__aarch64__)
